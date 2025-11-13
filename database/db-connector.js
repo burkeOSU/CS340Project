35d062fed2ffc1c24b155cc5e97f4d1bbe5d37e3
SPAN-THE-MAN <seanpatrickn@gmail.com>
2025-11-13 14:42:58 -0800

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -1,6 +1,6 @@
 //# Citation for this file:
 //# Date: 11/04/2025
-//# Based on:
+//# Copied from:
 //# Source URL: https://canvas.oregonstate.edu/courses/2017561/pages/exploration-web-application-technology-2?module_item_id=25645131
 
 
@@ -12,9 +12,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_nugents',
-    password          : '9328',
-    database          : 'cs340_nugents'
+    user              : 'cs340_[your_onid]',
+    password          : '[your_db_password]',
+    database          : 'cs340_[your_onid]'
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
